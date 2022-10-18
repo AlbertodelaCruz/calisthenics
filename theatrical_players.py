1e8978f2aa9a87d27e4f6ebf124420795c0ae19c
Alberto <acruz@alea-soluciones.com>
2022-10-18 19:33:22 +0200

    Rule 1 - One indentation level

--- theatrical_players.py
+++ theatrical_players.py
@@ -3,37 +3,53 @@ import math
 
 def statement(invoice, plays):
     total_amount = 0
-    volume_credits = 0
+    total_credits = 0
     result = f'Statement for {invoice["customer"]}\n'
 
-    def format_as_dollars(amount):
-        return f"${amount:0,.2f}"
-
     for perf in invoice['performances']:
-        play = plays[perf['playID']]
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
+        total_credits, result, total_amount = _calculate_perfomance_result(plays, perf, total_credits, result, total_amount)
 
-            this_amount += 300 * perf['audience']
-
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
-
-        # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
-        # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
-        # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
-
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
-    result += f'You earned {volume_credits} credits\n'
+    result += f'Amount owed is {_format_as_dollars(total_amount/100)}\nYou earned {total_credits} credits\n'
     return result
+
+def _calculate_perfomance_result(plays, perf, total_credits, result, total_amount):
+    play = plays[perf['playID']]
+    this_amount = _calculate_performance_amount(play, perf)
+    performance_credits = _calculate_performance_credits(perf, play)
+
+    result += f' {play["name"]}: {_format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+    total_amount += this_amount
+    total_credits += performance_credits
+    return total_credits, result, total_amount
+
+def _calculate_performance_credits(perf, play):
+    volume_credits = max(perf['audience'] - 30, 0)
+    if "comedy" == play["type"]:
+        volume_credits += math.floor(perf['audience'] / 5)
+    return volume_credits
+
+def _calculate_performance_amount(play, perf):
+    if not play['type'] in ['tragedy', 'comedy']:
+        raise ValueError(f'unknown type: {play["type"]}')
+    if play['type'] == "tragedy":
+        this_amount = _calculate_amount_for_tragedy_play(perf)
+    if play['type'] == "comedy":
+        this_amount = _calculate_amount_for_comedy_play(perf)
+    return this_amount
+
+def _calculate_amount_for_comedy_play(perf):
+    this_amount = 30000
+    if perf['audience'] > 20:
+        this_amount += 10000 + 500 * (perf['audience'] - 20)
+    this_amount += 300 * perf['audience']
+    return this_amount
+
+def _calculate_amount_for_tragedy_play(perf):
+    this_amount = 40000
+    if perf['audience'] > 30:
+        this_amount += 1000 * (perf['audience'] - 30)
+    return this_amount
+
+def _format_as_dollars(amount):
+    return f"${amount:0,.2f}"
+
