51c78cdb4419cdf7bcef92253fc14b53940d5a9c
Alberto <acruz@alea-soluciones.com>
2022-10-19 08:07:15 +0200

    rule 3 - encapsulate primitives: Credits

--- theatrical_players.py
+++ theatrical_players.py
@@ -12,46 +12,55 @@ class Amount:
         new_amount = self._amount + other.current()
         return Amount(new_amount)
 
+@dataclass
+class Credits:
+    _credits: int
+
+    def current(self):
+        return self._credits
+
+    def add(self, other):
+        new_credits = self._credits + other.current()
+        return Credits(new_credits)
+
 
 def statement(invoice, plays):
     amount = Amount(0)
-    total_credits = 0
+    credits = Credits(0)
     result = f'Statement for {invoice["customer"]}\n'
 
     for perf in invoice['performances']:
-        total_credits, result, amount = _calculate_perfomance_result(plays, perf, total_credits, result, amount)
+        credits, result, amount = _calculate_perfomance_result(plays, perf, credits, result, amount)
 
-    result += f'Amount owed is {_format_as_dollars(amount.current()/100)}\nYou earned {total_credits} credits\n'
+    result += f'Amount owed is {_format_as_dollars(amount.current()/100)}\nYou earned {credits.current()} credits\n'
     return result
 
-def _calculate_perfomance_result(plays, perf, total_credits, result, amount):
+def _calculate_perfomance_result(plays, perf, credits, result, amount):
     play = plays[perf['playID']]
     performance_amount = _calculate_performance_amount(play, perf)
     performance_credits = _calculate_performance_credits(perf, play)
 
     result += f' {play["name"]}: {_format_as_dollars(performance_amount.current()/100)} ({perf["audience"]} seats)\n'
     amount = amount.add(performance_amount)
-    total_credits += performance_credits
-    return total_credits, result, amount
+    credits = credits.add(performance_credits)
+    return credits, result, amount
 
 def _calculate_performance_credits(perf, play):
-    volume_credits = max(perf['audience'] - 30, 0)
-    volume_credits = _calculate_extra_credits_for_comedy(play, perf, volume_credits)
-    return volume_credits
+    credits = Credits(max(perf['audience'] - 30, 0))
+    return credits.add(_calculate_extra_credits_for_comedy(play, perf))
 
-def _calculate_extra_credits_for_comedy(play, perf, volume_credits):
+def _calculate_extra_credits_for_comedy(play, perf):
     if "comedy" == play["type"]:
-        volume_credits += math.floor(perf['audience'] / 5)
-    return volume_credits
+        return Credits(math.floor(perf['audience'] / 5))
+    return Credits(0)
 
 def _calculate_performance_amount(play, perf):
     if not play['type'] in ['tragedy', 'comedy']:
         raise ValueError(f'unknown type: {play["type"]}')
     if play['type'] == "tragedy":
-        performance_amount = _calculate_amount_for_tragedy_play(perf)
+        return _calculate_amount_for_tragedy_play(perf)
     if play['type'] == "comedy":
-        performance_amount = _calculate_amount_for_comedy_play(perf)
-    return performance_amount
+        return _calculate_amount_for_comedy_play(perf)
 
 def _calculate_amount_for_comedy_play(perf):
     return Amount(30000)\
@@ -74,4 +83,3 @@ def _calculate_extra_amount_for_tragedy_high_audience(perf):
 
 def _format_as_dollars(amount):
     return f"${amount:0,.2f}"
-
